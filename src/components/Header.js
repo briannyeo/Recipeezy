f8f449336aef585256ccc43f0c606a7ddb1bb983
brianyeo <brianyeo1995@gmail.com>
2022-02-20 16:21:52 +0800

    imported reactrouterdom

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <table>
+        <thead>Recipes</thead>
+        <td>Ingredients</td>
+      </table>
+    </div>
+  );
+};
+
+export default Header;
