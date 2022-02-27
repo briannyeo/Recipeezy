bc3b48f72a0b1569ed4425ecd426d14bda5cfe7b
brianyeo <brianyeo1995@gmail.com>
2022-02-27 19:33:09 +0800

    removed setToggleSearch

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -11,7 +11,6 @@ const SearchBar = (props) => {
   const handleSubmit = () => {
     if (searchWord !== "") {
       setRecipeWord(searchWord);
-      //setToggleSearch(!toggleSearch);
     } else return;
   };
 
