1a5854a41540e3ce37c50e6910c78b0d5c9033d2
brianyeo <brianyeo1995@gmail.com>
2022-03-01 13:24:55 +0800

    removed axios call from searchbar - added Link to results page

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,34 +1,18 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState } from "react";
 import "./SearchBar.css";
-import axios from "axios";
 import searchIcon from "../../images/searchicon.png";
 import { Link } from "react-router-dom";
 
 const SearchBar = (props) => {
-  const [recipeWord, setRecipeWord] = useState("");
   const [searchWord, setSearchWord] = useState("");
+  // const [searchParam, setSearchParam] = useSearchParams("");
 
-  const handleSubmit = () => {
-    if (searchWord !== "") {
-      setRecipeWord(searchWord);
-    } else return;
-  };
+  // const navigate = useNavigate();
 
-  const appId = "e07457e3";
-  const appKey = "126c04352c3086f78d428c7ce21556d0";
-
-  useEffect(() => {
-    axios
-      .get(
-        `https://api.edamam.com/api/recipes/v2?type=public&q=${recipeWord}&app_id=${appId}&app_key=${appKey}`
-      )
-      .then((res) => {
-        props.setSearchData(res.data.hits);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, [recipeWord]);
+  // const handleSubmit = () => {
+  //   console.log(searchParam);
+  //   setSearchParam({ q: searchWord });
+  // };
 
   return (
     <div className="searchForm">
@@ -38,11 +22,11 @@ const SearchBar = (props) => {
         }}
         placeholder="Search for Recipes"
       />
-      <button type="submit" onClick={handleSubmit}>
-        <Link to="/results">
+      <Link to={`results/${searchWord}`}>
+        <button type="submit">
           <img className="searchiconimg" src={searchIcon} alt="search" />
-        </Link>
-      </button>
+        </button>
+      </Link>
     </div>
   );
 };
