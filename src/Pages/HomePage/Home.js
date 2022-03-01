438e1ac088d162398d1af2c8d4ae194199eb36bd
brianyeo <brianyeo1995@gmail.com>
2022-03-01 13:32:08 +0800

    removed useContext and state

--- src/Pages/HomePage/Home.js
+++ src/Pages/HomePage/Home.js
@@ -1,18 +1,12 @@
-import React, { useState, useContext } from "react";
+import React, { useState } from "react";
 import "./Home.css";
 import SearchBar from "../../components/SearchBar/SearchBar";
-import SearchResults from "../SearchResultsPage/SearchResultsPage";
 
 const Home = () => {
-  //window.localStorage.setItem("a", 1);
-  // const [searchData, setSearchData] = useState();
   return (
-    // <SearchContext.Provider value={searchData}>
     <div className="searchBar">
       <SearchBar />
-      {/* <SearchResults searchData={searchData} setSearchData={setSearchData} /> */}
     </div>
-    // </SearchContext.Provider>
   );
 };
 
