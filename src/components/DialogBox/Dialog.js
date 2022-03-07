98368e431aafa06b0b1c8afd142a4c3f4179cdfc
brianyeo <brianyeo1995@gmail.com>
2022-03-07 18:07:43 +0800

    reformatted dialog info

--- src/components/DialogBox/Dialog.js
+++ src/components/DialogBox/Dialog.js
@@ -17,19 +17,25 @@ export default function Dialog(props) {
       <Modal.Body>
         <h4>Macros:</h4>
         <ul>
-          <li>Calories - {props.calories}</li>
-          <li>Protein - {props.protein}</li>
-          <li>Fats - {props.fats}</li>
-          <li>Carbs - {props.carbs}</li>
+          <li>Calories - {props.calories} kCal</li>
+          <li>Protein - {props.protein} g</li>
+          <li>Fats - {props.fats} g</li>
+          <li>Carbs - {props.carbs} g</li>
         </ul>
         <h4>Ingredients:</h4>
         {props.ingredientlines.map((e, index) => (
           <li key={index}>{e}</li>
         ))}
-        <h4>Learn to cook:</h4>
-        <a href={props.instructions} target="_blank" rel="noopener noreferrer">
-          Full Recipe
-        </a>
+        <h5>
+          For full recipe and instructions, click{" "}
+          <a
+            href={props.instructions}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            here
+          </a>
+        </h5>
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={props.handleaddrecipe}>Add Recipe</Button>
