5392215c8b67cf3b960b30f29c32b109861d8f1d
brianyeo <brianyeo1995@gmail.com>
2022-03-08 17:21:33 +0800

    added About page

--- src/Pages/AboutPage/About.js
+++ src/Pages/AboutPage/About.js
@@ -2,7 +2,37 @@ import React from "react";
 import "./About.css";
 
 const About = () => {
-  return <div className="about">About</div>;
+  return (
+    <div className="about">
+      <div className="aboutHeader">Recipeezy</div>
+      <div className="aboutDetails">
+        <ul>
+          {" "}
+          <li>
+            Ever walked around aimlessly in a supermarket not knowing what
+            ingredients to buy?
+          </li>{" "}
+          <li>
+            Or how about simply needing that touch of inspiration when answering
+            the age-old question "What should I cook today?"
+          </li>{" "}
+        </ul>
+        <div className="recipeezyDetails">
+          How Recipeezy works:
+          <ol>
+            <li>
+              Type an ingredient into the "Search" bar (such as "Chicken")
+            </li>
+            <li>Add your favourite recipes to "Planned Meals"</li>
+            <li>
+              Refer to your "Grocery List" the next time you visit a grocery
+              store!{" "}
+            </li>
+          </ol>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
