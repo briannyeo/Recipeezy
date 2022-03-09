3c6052e64deed8951c94d472171fa3f32dba02fb
Min Shan <liewminshan@gmail.com>
2022-03-09 12:48:16 +0800

    Add context for background styles

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./Main.css";
 import Home from "./HomePage/Home";
@@ -9,31 +9,42 @@ import SearchResults from "./SearchResultsPage/SearchResultsPage";
 import RecipeDetails from "./RecipeDetailsPage/RecipeDetails";
 import NoMatch from "./NoMatchPage";
 import SearchBarBS from "../components/SearchBar/SearchBarBS";
-import background from "../images/recipeezyhomepage.jpeg";
+import background from '../images/recipeezyhomepage.jpeg';
+import { BackgroundStylesContext } from "../contexts/BackgroundStylesContext";
 
 function Main() {
+  const [backgroundStyles, setBackgroundStyles] = useState({
+    height: '100vh',
+    width: '100vw',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center center',
+    background: `url(${background})`,
+    backgroundAttachment: 'fixed',
+    position: "fixed",
+    zIndex: -1,
+  });
+
   return (
     <main
       className="main"
-      style={{
-        backgroundSize: "cover",
-        backgroundPosition: "center center",
-        background: `url(${background})`,
-        backgroundAttachment: "fixed",
-      }}
     >
-      <Routes>
-        <Route path="Recipeezy" element={<Home />}>
-          <Route index element={<SearchBarBS />} />
-          <Route path="grocerylist" element={<GroceryList />} />
-          <Route path="plannedmeals" element={<PlannedMeals />} />
-          <Route path="about" element={<About />} />
-          <Route path="results" element={<SearchResults />} />
-          <Route path="recipe/:recipetitle" element={<RecipeDetails />} />
+      <BackgroundStylesContext.Provider value={{ backgroundStyles, setBackgroundStyles }} > 
+        <div
+          style={{...backgroundStyles}}
+        />
+        <Routes>
+          <Route path="Recipeezy" element={<Home />}>
+            <Route index element={<SearchBarBS />} />
+            <Route path="grocerylist" element={<GroceryList />} />
+            <Route path="plannedmeals" element={<PlannedMeals />} />
+            <Route path="about" element={<About />} />
+            <Route path="results" element={<SearchResults />} />
+            <Route path="recipe/:recipetitle" element={<RecipeDetails />} />
+            <Route path="*" element={<NoMatch />} />
+          </Route>
           <Route path="*" element={<NoMatch />} />
-        </Route>
-        <Route path="*" element={<NoMatch />} />
-      </Routes>
+        </Routes>
+      </BackgroundStylesContext.Provider>
     </main>
   );
 }
