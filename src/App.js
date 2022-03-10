f103d207c35ec650cda72cf03bb20de16f9ffb5a
brianyeo <brianyeo1995@gmail.com>
2022-03-10 18:47:21 +0800

    testing firebase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import "./App.css";
-
 import NavBar from "./components/NavBar/NavBar";
-
 import Main from "./Pages/Main";
+import { useState, useRef } from "react";
+
+import { storage } from "./firebase";
+import { getDownloadURL } from "firebase/storage";
 
 function App() {
+  // const [progress, setProgress] = useState(0);
+
+  // const uploadFiles = (file) => {
+  //   if (!file) return;
+  //   const storageRef = ref(storage, `/files/${file.name}`);
+  //   const uploadTask = uploadBytesResumable(storageRef, file);
+
+  //   uploadTask.on(
+  //     "state_changed",
+  //     (snapshot) => {
+  //       const prog = Math.round(
+  //         (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+  //       );
+  //       setProgress(prog);
+  //     },
+  //     (err) => console.log(err),
+  //     () => {
+  //       getDownloadURL(uploadTask.snapshot.ref).then((url) => console.log(url));
+  //     }
+  //   );
+  // };
+
   return (
     <div className="app">
       <NavBar />
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+// import { initializeApp } from "firebase/app";
+// import { getStorage } from "firebase/storage";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA3ZBLEhPanaXx2YgONja_8F1hq48nO2_M",
+//   authDomain: "recipeezy-25170.firebaseapp.com",
+//   projectId: "recipeezy-25170",
+//   storageBucket: "recipeezy-25170.appspot.com",
+//   messagingSenderId: "127999015271",
+//   appId: "1:127999015271:web:297c1e15e6cb57cb6ad7c2",
+//   measurementId: "G-TZ0CSDL6RB",
+// };
+
+// export const app = initializeApp(firebaseConfig);
+// export const storage = getStorage(a);
